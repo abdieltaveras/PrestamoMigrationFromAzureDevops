551abfaf7aad8cbd987d510a16e54a39215c616a
abdiel taveras <abdieltaveras@hotmail.com>
2020-05-18 09:35:29 -0400

    testing git message when pulling

--- PrestamosMVC5/Controllers/TasaInteresController.cs
+++ PrestamosMVC5/Controllers/TasaInteresController.cs
@@ -22,6 +22,7 @@ namespace PrestamosMVC5.Controllers
         public ActionResult Index()
         {
             TasaInteresVM modelo = new TasaInteresVM();
+            var temp = "probando conflict merge";
 
             modelo.ListaTasaInteres = BLLPrestamo.Instance.TasasInteresGet(new TasaInteresGetParams { IdNegocio = pcpUserIdNegocio });
             //ViewBag.listaInteres = intereses;
