fde7e4c42b40b30a172edfa66aba2e9a16f308e9
Bryan <bryan.osvaldo1@gmail.com>
2020-07-28 10:34:59 -0400

    [FIX] Add tasa interes

--- PrestamosMVC5/Controllers/TasaInteresController.cs
+++ PrestamosMVC5/Controllers/TasaInteresController.cs
@@ -38,7 +38,7 @@ namespace PrestamosMVC5.Controllers
             
             try
             {
-               // BLLPrestamo.Instance.TasaInteresInsUpd(interes.TasaInteres);
+               BLLPrestamo.Instance.TasaInteresInsUpd(interes.TasaInteres);
             }
             catch (Exception ex)
             {
