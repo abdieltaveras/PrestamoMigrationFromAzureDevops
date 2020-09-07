c07ba3088b1d5106441777e7eee123988819cc9f
Alvaro Martinez <armpmartinez@gmail.com>
2020-09-07 17:12:04 -0400

    TestMethond de servicios de correo

---
+++ PrestamosMVC5Tests/SiteUtils/EmailServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrestamosMVC5.SiteUtils;
+
+namespace PrestamosMVC5Tests.SiteUtils
+{
+    [TestClass]
+    public class EmailServiceTest
+    {
+        [TestMethod]
+        public void TestSendMail()
+        {
+            var result = EmailService.SendMail("armpmartinez@gmail.com", "prueba", "Una prueba");
+
+            Assert.IsTrue(result);
+        }
+    }
+}
