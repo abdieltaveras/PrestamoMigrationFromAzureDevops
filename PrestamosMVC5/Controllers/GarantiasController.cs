5bbe854af4fa9c78a296f9b71001267af3a3af0a
Luis Bill Heskey Rodriguez <luisbillr@gmail.com>
2020-10-21 21:37:06 -0400

    [Add] Adding Images Control to Garantia Controller

--- PrestamosMVC5/Controllers/GarantiasController.cs
+++ PrestamosMVC5/Controllers/GarantiasController.cs
@@ -46,9 +46,9 @@ namespace PrestamosMVC5.Controllers
             {
                 var datosGarantia = GetGarantia(id).DataList.FirstOrDefault();
                 datos.Garantia = datosGarantia;
-                datos.Garantia.DetallesJSON = datosGarantia.Detalles.ToType<DetalleGarantia>();
+                datos.Garantia.DetallesJSON = datosGarantia.DetallesJSON;
             }
-            
+
             ////*******Imagenes Garantia****//
             //var garantiaTempData = GetValueFromTempData<Garantia>("Garantia");
             //var imagen1GarantiaFileName = Utils.SaveFile(Server.MapPath(SiteDirectory.ImagesForGarantia), garantia.image1PreviewValue);
@@ -57,7 +57,7 @@ namespace PrestamosMVC5.Controllers
             //var imagen4GarantiaFileName = Utils.SaveFile(Server.MapPath(SiteDirectory.ImagesForGarantia), garantia.image4PreviewValue);
             //garantia.Garantia.Imagen1FileName = GeneralUtils.GetNameForFile(imagen1GarantiaFileName, garantia.image1PreviewValue, garantiaTempData.Imagen1FileName);
             //garantia.Garantia.Imagen2FileName = GeneralUtils.GetNameForFile(imagen2GarantiaFileName, garantia.image2PreviewValue, garantiaTempData.Imagen2FileName);
-            
+
             //garantia.Garantia.Imagen3FileName = GeneralUtils.GetNameForFile(imagen3GarantiaFileName, garantia.image3PreviewValue, garantiaTempData.Imagen3FileName);
             //garantia.Garantia.Imagen4FileName = GeneralUtils.GetNameForFile(imagen4GarantiaFileName, garantia.image4PreviewValue, garantiaTempData.Imagen4FileName);
             return View(datos);
@@ -73,32 +73,39 @@ namespace PrestamosMVC5.Controllers
 
             List<ResponseMessage> listaMensajes = new List<ResponseMessage>();
 
+            //var clienteTempData = GetValueFromTempData<Garantia>("Garantia");
+            //var imagen1ClienteFileName = Utils.SaveFile(Server.MapPath(SiteDirectory.ImagesForClientes), garantiavm.image1PreviewValue);
+            //var imagen2ClienteFileName = Utils.SaveFile(Server.MapPath(SiteDirectory.ImagesForClientes), clienteVm.image2PreviewValue);
+            //clienteVm.Cliente.Imagen1FileName = GeneralUtils.GetNameForFile(imagen1ClienteFileName, clienteVm.image1PreviewValue, clienteTempData.Imagen1FileName);
+
+            //clienteVm.Cliente.Imagen2FileName = GeneralUtils.GetNameForFile(imagen2ClienteFileName, clienteVm.image2PreviewValue, clienteTempData.Imagen2FileName);
+
             //*******Imagenes Garantia****//
-            //var garantiaTempData = GetValueFromTempData<Garantia>("Garantia");
-            //var imagen1GarantiaFileName = Utils.SaveFile(Server.MapPath(SiteDirectory.ImagesForGarantia), garantiavm.image1PreviewValue);
-            //var imagen2GarantiaFileName = Utils.SaveFile(Server.MapPath(SiteDirectory.ImagesForGarantia), garantiavm.image2PreviewValue);
-            //var imagen3GarantiaFileName = Utils.SaveFile(Server.MapPath(SiteDirectory.ImagesForGarantia), garantiavm.image3PreviewValue);
-            //var imagen4GarantiaFileName = Utils.SaveFile(Server.MapPath(SiteDirectory.ImagesForGarantia), garantiavm.image4PreviewValue);
-            //garantia.Imagen1FileName = GeneralUtils.GetNameForFile(imagen1GarantiaFileName, garantiavm.image1PreviewValue, garantiaTempData.Imagen1FileName);
-            //garantia.Imagen2FileName = GeneralUtils.GetNameForFile(imagen2GarantiaFileName, garantiavm.image2PreviewValue, garantiaTempData.Imagen2FileName);
-            //garantia.Imagen3FileName = GeneralUtils.GetNameForFile(imagen3GarantiaFileName, garantiavm.image3PreviewValue, garantiaTempData.Imagen3FileName);
-            //garantia.Imagen4FileName = GeneralUtils.GetNameForFile(imagen4GarantiaFileName, garantiavm.image4PreviewValue, garantiaTempData.Imagen4FileName);
-            if (!ModelState.IsValid)
-            {
-                foreach (var errors in ModelState.Values)
-                {
-                    foreach (var error in errors.Errors)
-                    {
-                        listaMensajes.Add(new ResponseMessage()
-                        {
-                            Tipo = ResponseMessage.TYPE_ERROR,
-                            Mensaje = error.ErrorMessage
-                        });
-                    }
-                }
-                TempData["list"] = listaMensajes;
-                return RedirectToAction("CreateOrEdit", new { @ListaMensajes = listaMensajes, @garantia = garantia });
-            }
+            var garantiaTempData = GetValueFromTempData<Garantia>("Garantia");
+            var imagen1GarantiaFileName = Utils.SaveFile(Server.MapPath(SiteDirectory.ImagesForGarantia), garantiavm.image1PreviewValue);
+            var imagen2GarantiaFileName = Utils.SaveFile(Server.MapPath(SiteDirectory.ImagesForGarantia), garantiavm.image2PreviewValue);
+            var imagen3GarantiaFileName = Utils.SaveFile(Server.MapPath(SiteDirectory.ImagesForGarantia), garantiavm.image3PreviewValue);
+            var imagen4GarantiaFileName = Utils.SaveFile(Server.MapPath(SiteDirectory.ImagesForGarantia), garantiavm.image4PreviewValue);
+            garantia.Imagen1FileName = GeneralUtils.GetNameForFile(imagen1GarantiaFileName, garantiavm.image1PreviewValue, garantiaTempData.Imagen1FileName);
+            garantia.Imagen2FileName = GeneralUtils.GetNameForFile(imagen2GarantiaFileName, garantiavm.image2PreviewValue, garantiaTempData.Imagen2FileName);
+            garantia.Imagen3FileName = GeneralUtils.GetNameForFile(imagen3GarantiaFileName, garantiavm.image3PreviewValue, garantiaTempData.Imagen3FileName);
+            garantia.Imagen4FileName = GeneralUtils.GetNameForFile(imagen4GarantiaFileName, garantiavm.image4PreviewValue, garantiaTempData.Imagen4FileName);
+            //if (!ModelState.IsValid)
+            //{
+            //    foreach (var errors in ModelState.Values)
+            //    {
+            //        foreach (var error in errors.Errors)
+            //        {
+            //            listaMensajes.Add(new ResponseMessage()
+            //            {
+            //                Tipo = ResponseMessage.TYPE_ERROR,
+            //                Mensaje = error.ErrorMessage
+            //            });
+            //        }
+            //    }
+            //    TempData["list"] = listaMensajes;
+            //    return RedirectToAction("CreateOrEdit", new { @ListaMensajes = listaMensajes, @garantia = garantia });
+            //}
 
             try
             {
