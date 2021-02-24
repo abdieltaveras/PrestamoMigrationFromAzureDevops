c6bd542303a22ab6ca7010f06b04b12d8829857d
abdiel taveras <abdieltaveras@hotmail.com>
2021-02-23 21:58:54 -0400

    [add] marcas controller starting

--- PrestamoWS/Controllers/ClientesController.cs
+++ PrestamoWS/Controllers/ClientesController.cs
@@ -19,6 +19,48 @@ namespace PrestamoWS.Controllers
             var data = BLLPrestamo.Instance.GetClientes(new ClienteGetParams());
             return data;
         }
-        
+
+        [HttpGet]
+        public IEnumerable<Cliente> GetById(int id)
+        {
+            var data = BLLPrestamo.Instance.GetClientes(new ClienteGetParams {IdCliente=id });
+            return data;
+        }
+        [HttpGet]
+        public IEnumerable<Cliente> GetByParams(string noIdentificacion, string nombre, string apellido)
+        {
+            var data = BLLPrestamo.Instance.GetClientes(new ClienteGetParams {  });
+            return data;
+        }
+
+        [HttpGet]
+        public IEnumerable<Cliente> GetByParams2(ClienteGetParams param)
+        {
+            var data = BLLPrestamo.Instance.GetClientes(new ClienteGetParams { });
+            return data;
+        }
+
+        [HttpGet]
+        public IEnumerable<Cliente> BuscarClientes(string searchToText, bool CargarImagenesClientes)
+        {
+            var clientes = searchCliente(searchToText, CargarImagenesClientes);
+            return clientes;
+        }
+        private IEnumerable<Cliente> searchCliente(string searchToText, bool CargarImagenesClientes)
+        {
+            IEnumerable<Cliente> clientes = null;
+
+            clientes = BLLPrestamo.Instance.SearchCliente(new BuscarClienteParams { TextToSearch = searchToText, IdNegocio = 1 });
+
+            if (CargarImagenesClientes)
+            {
+                foreach (var cliente in clientes)
+                {
+                    //cliente.Imagen1FileName = Url.Content(SiteDirectory.ImagesForClientes + "/" + cliente.Imagen1FileName);
+                }
+            }
+
+            return clientes;
+        }
     }
 }
---
+++ PrestamoWS/Controllers/MarcasController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrestamoBLL.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrestamoWS.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class MarcasController : Controller
+    {
+        [HttpGet]
+        public Marca Get()
+        {
+            return new Marca();
+        }
+    }
+}
