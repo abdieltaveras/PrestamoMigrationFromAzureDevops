d83c83add7597e29f332707991a64e743ca38734
LUIS BILL HESKEY RODRIGUEZ <luisbillr@hotmail.com>
2021-03-19 06:31:34 -0400

    [Add]Garantias

---
+++ PrestamoBlazorApp/Pages/Garantias/Garantias.cs
@@ -0,0 +1,70 @@
+﻿using PrestamoBLL.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PrestamoBlazorApp.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace PrestamoBlazorApp.Pages.Garantias
+{
+    public partial class Garantias
+    {
+        [Inject]
+        IJSRuntime jsRuntime { get; set; }
+        JsInteropUtils JsInteropUtils { get; set; } = new JsInteropUtils();
+        [Inject]
+        GarantiasService GarantiasService { get; set; }
+        IEnumerable<Garantia> garantias { get; set; } = new List<Garantia>();
+        [Parameter]
+        public Garantia Garantia { get; set; } 
+        bool loading = false;
+        GarantiaGetParams SearchGarantia { get; set; } = new GarantiaGetParams();
+        void Clear() => garantias = null;
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            this.Garantia = new Garantia();
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            garantias = await GarantiasService.GetWithPrestamo(new BuscarGarantiaParams { IdNegocio = 1, Search = "-1"});
+        }
+        async Task GetGarantias()
+        {
+            loading = true;
+            var param = new GarantiaGetParams { IdNegocio = 1,IdGarantia = 1 };
+            garantias = await GarantiasService.Get(param);
+            loading = false;
+        }
+
+        //async Task GetAll()
+        //{
+        //    loading = true;
+        //    Garantias = await GarantiasService.GetAll();
+        //    loading = false;
+        //}
+
+        async Task SaveGarantia()
+        {
+            await GarantiasService.SaveGarantia(this.Garantia);
+        }
+        void CreateOrEdit(int idGarantia = -1)
+        {
+            if (idGarantia > 0)
+            {
+                this.Garantia = garantias.Where(m => m.IdGarantia == idGarantia).FirstOrDefault();
+            }
+            else
+            {
+                this.Garantia = new Garantia();
+            }
+            JsInteropUtils.ShowModal(jsRuntime, "#ModalCreateOrEdit");
+        }
+        void RaiseInvalidSubmit()
+        {
+            
+        }
+    }
+}
---
+++ PrestamoBlazorApp/Services/GarantiasService.cs
@@ -0,0 +1,44 @@
+
+using Microsoft.Extensions.Configuration;
+using PrestamoBLL.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PrestamoBlazorApp.Services
+{
+    public class GarantiasService : ServiceBase
+    {
+        string apiUrl = "api/garantias";
+        public async Task<IEnumerable<Garantia>> GetWithPrestamo(BuscarGarantiaParams search)
+        {
+            //string searchToText = search.Search;
+            var result = await GetAsync<Garantia>(apiUrl + "/GetWithPrestamo", search);
+            return result;
+        }
+
+        public async Task<IEnumerable<Garantia>> Get(GarantiaGetParams getParams)
+        {
+            var result =  await GetAsync<GarantiaConMarcaYModeloYPrestamos>(apiUrl, getParams);
+            return result;
+        }
+        public GarantiasService(IHttpClientFactory clientFactory, IConfiguration configuration) : base(clientFactory, configuration)
+        {
+
+        }
+
+        public async Task SaveGarantia(Garantia Garantia)
+        {
+            try
+            {
+                await PostAsync<Garantia>(apiUrl, Garantia);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al guardar", ex);
+            }
+
+        }
+    }
+}
