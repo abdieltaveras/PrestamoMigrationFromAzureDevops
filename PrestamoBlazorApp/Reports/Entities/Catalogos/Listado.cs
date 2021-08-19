988fabd712dd8063388a32e953978b13319defb1
LUIS BILL HESKEY RODRIGUEZ <luisbillr@hotmail.com>
2021-08-19 09:20:15 -0400

    [add] reports

---
+++ PrestamoBlazorApp/Reports/Entities/Catalogos/Listado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrestamoBlazorApp.Reports.Entities.Catalogos
+{
+    public class Listado
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Catalogo { get; set; }
+    }
+}
---
+++ PrestamoBlazorApp/Reports/Entities/Marcas/Listado.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrestamoBlazorApp.Reports.Entities.Marcas
+{
+    public class Listado
+    {
+    }
+}
