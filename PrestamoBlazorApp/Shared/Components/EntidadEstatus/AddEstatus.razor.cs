f806fb3828b71ea9679c0757517b11b99f709a0f
abdieltaveras <abdieltaveras@hotmail.com>
2022-10-04 23:06:28 -0400

    [add] helpertext and title to help user undestand the component

--- PrestamoBlazorApp/Shared/Components/EntidadEstatus/AddEstatus.razor.cs
+++ PrestamoBlazorApp/Shared/Components/EntidadEstatus/AddEstatus.razor.cs
@@ -13,37 +13,16 @@ namespace PrestamoBlazorApp.Shared.Components.EntidadEstatus
     {
         [Inject]
         EntidadEstatusService EntidadEstatusService { get; set; }
-        private bool resetValueOnEmptyText;
-        private bool coerceText;
-        private bool coerceValue;
-        private string _value2 { get; set; }
-        private string value2 { get { return _value2; } set { _value2 = value; OnSelect(value).GetAwaiter(); } }
+
+        private string _SelectedEstatus { get; set; }
+        private string SelectedEstatus { get { return _SelectedEstatus; } set { _SelectedEstatus = value; OnSelect(value).GetAwaiter(); } }
         private List<string> lstEstatus { get; set; } = new List<string>();
         [Parameter]
         public EventCallback<string> OnSelectItem { get; set; }
-
-        //private string[] states =
-        //{
-        //    "Alabama", "Alaska", "American Samoa", "Arizona",
-        //    "Arkansas", "California", "Colorado", "Connecticut",
-        //    "Delaware", "District of Columbia", "Federated States of Micronesia",
-        //    "Florida", "Georgia", "Guam", "Hawaii", "Idaho",
-        //    "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky",
-        //    "Louisiana", "Maine", "Marshall Islands", "Maryland",
-        //    "Massachusetts", "Michigan", "Minnesota", "Mississippi",
-        //    "Missouri", "Montana", "Nebraska", "Nevada",
-        //    "New Hampshire", "New Jersey", "New Mexico", "New York",
-        //    "North Carolina", "North Dakota", "Northern Mariana Islands", "Ohio",
-        //    "Oklahoma", "Oregon", "Palau", "Pennsylvania", "Puerto Rico",
-        //    "Rhode Island", "South Carolina", "South Dakota", "Tennessee",
-        //    "Texas", "Utah", "Vermont", "Virgin Island", "Virginia",
-        //    "Washington", "West Virginia", "Wisconsin", "Wyoming",
-        //};
-
         protected override async Task OnInitializedAsync()
         {
             await GetStatus();
-            //await base.OnInitializedAsync();
+            await base.OnInitializedAsync();
         }
 
         private async Task OnSelect(string val)
@@ -60,17 +39,17 @@ namespace PrestamoBlazorApp.Shared.Components.EntidadEstatus
             }
             StateHasChanged();
         }
-        private async Task<IEnumerable<string>> Search1(string value)
+        private async Task<IEnumerable<string>> EstatusListSearch(string value)
         {
-            await GetStatus();
+            // await GetStatus(); no recargaremos a menos que sea necesario mediante un refresh
             //return lstEstatus;
             // In real life use an asynchronous function for fetching data from an api.
             //await Task.Delay(5);
-
+            await Task.Delay(5);
             // if text is null or empty, show complete list
-            if (string.IsNullOrEmpty(value))
-                return lstEstatus;
+            if (string.IsNullOrEmpty(value)) return lstEstatus; //return new String[0]; 
             return lstEstatus.Where(x => x.Contains(value, StringComparison.InvariantCultureIgnoreCase));
         }
+
     }
 }
