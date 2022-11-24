22063e512d629fc3016929a5cb5d6b551ab70bfe
Luis Heskey <luis_heskey@claro.com.do>
2022-11-23 21:15:37 -0400

    [add] PrestamoEstatusTests and ClienteEstatusTests

---
+++ PrestamoDB/dbo/Stored Procedures/spGetPrestamoEstatus.sql
@@ -0,0 +1,10 @@
+﻿CREATE proc [dbo].[spGetPrestamoEstatus]
+@IdPrestamo int = -1,
+@IdEstatus int = -1
+as
+SELECT ce.IdPrestamo,ce.IdEstatus, ce.Activo,e.Name EstatusName,e.Description EstatusDescription
+FROM tblPrestamoEstatus ce
+INNER JOIN tblEstatus e
+ON ce.IdEstatus =e.IdEstatus
+WHERE (@IdEstatus  = -1 or ce.IdEstatus = @IdEstatus)
+and (@IdPrestamo  = -1 or ce.IdPrestamo = @IdPrestamo)
