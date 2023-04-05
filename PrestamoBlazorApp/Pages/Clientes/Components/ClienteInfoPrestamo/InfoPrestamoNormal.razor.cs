caa3756bd4a7c7e5f32f710ab098f382aaff3900
Luis Heskey <luis_heskey@claro.com.do>
2023-04-05 07:28:54 -0400

    [add] Alert for pending prestamos

--- PrestamoBlazorApp/Pages/Clientes/Components/ClienteInfoPrestamo/InfoPrestamoNormal.razor.cs
+++ PrestamoBlazorApp/Pages/Clientes/Components/ClienteInfoPrestamo/InfoPrestamoNormal.razor.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Components;
 using MudBlazor;
+using PrestamoBlazorApp.Pages.Prestamos;
+using PrestamoBlazorApp.Pages.Prestamos.Components;
 using PrestamoBlazorApp.Services;
+using PrestamoBlazorApp.Shared;
 using PrestamoBlazorApp.Shared.Components.Prestamos;
 using PrestamoEntidades;
 using System;
@@ -11,8 +14,10 @@ using System.Threading.Tasks;
 namespace PrestamoBlazorApp.Pages.Clientes.Components.ClienteInfoPrestamo
 {
     //  esto es un componente, pero lo puse en una pagina para desarrollarlo sin tener que perder tiempo invocando
-    public partial class InfoPrestamoNormal
+    public partial class InfoPrestamoNormal:BaseForList
     {
+        [Inject]
+        private PrestamosService PrestamosService { get; set; }
         [Inject]
         IDialogService DialogService { get; set; }
         private string SearchText { get; set; }
@@ -22,7 +27,28 @@ namespace PrestamoBlazorApp.Pages.Clientes.Components.ClienteInfoPrestamo
         PrestamoClienteUI _Prestamo { get; set; } = new PrestamoClienteUI();
         IEnumerable<GarantiaConMarcaYModelo> _Garantias { get; set; } = new List<GarantiaConMarcaYModelo>();
 
-
+        protected override async Task OnInitializedAsync()
+        {
+           
+        }
+        private async Task DialogPrestamosList(int id)
+        {
+            var prestamosCliente = await PrestamosService.GetPrestamoClienteUI(new PrestamoClienteUIGetParam { IdCliente = _Prestamo.IdCliente });
+            if (prestamosCliente.Count() > 0)
+            {
+                await NotifyMessageBySnackBar($"Este Cliente Tiene Mas Prestamos", Severity.Error);
+                var parameters = new DialogParameters { ["IdCliente"] = id };
+                DialogOptions dialogOptions = new DialogOptions { MaxWidth = MaxWidth.Medium, FullWidth = true, CloseButton = true };
+                var dialog = DialogService.Show<ClientePrestamosList>($"Otros Prestamos De {_Prestamo.NombreCompleto}", parameters, dialogOptions);
+                var result = await dialog.Result;
+                if (!result.Cancelled)
+                {
+                    _Prestamo = (PrestamoClienteUI)result.Data;
+                    await GetGarantias(_Prestamo.IdPrestamo);
+                }
+            }
+       
+        }
         private async Task DialogSearchPrestamo()
         {
             string[] cols = { "Nombres", "Apellidos" };
@@ -35,6 +61,7 @@ namespace PrestamoBlazorApp.Pages.Clientes.Components.ClienteInfoPrestamo
             {
                 _Prestamo = (PrestamoClienteUI)result.Data;
                 await GetGarantias(_Prestamo.IdPrestamo);
+                await DialogPrestamosList(_Prestamo.IdCliente);
             }
         }
         private async Task GetGarantias(int IdPrestamo)
---
+++ PrestamoBlazorApp/Pages/Prestamos/Components/ClientePrestamosList.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using PrestamoBlazorApp.Services;
+using PrestamoEntidades;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PrestamoBlazorApp.Pages.Prestamos.Components
+{
+    public partial class ClientePrestamosList
+    {
+        [CascadingParameter] MudDialogInstance MudDialog { get; set; }
+        IEnumerable<PrestamoClienteUI> prestamos = new List<PrestamoClienteUI>();
+        [Inject] PrestamosService PrestamosService { get; set; }
+        [Parameter]
+        public int IdCliente { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            //await Handle_GetDataForList(GetPrestamos);
+            //await base.OnInitializedAsync();
+            await GetPrestamos();
+        }
+
+        private async Task GetPrestamos()
+        {
+            //prestamos = await PrestamosService.GetAsync(new PrestamosGetParams { idCliente = IdCliente});
+            prestamos = await PrestamosService.GetPrestamoClienteUI(new PrestamoClienteUIGetParam { IdCliente = IdCliente});
+            //totalPrestamos = prestamos.Count();
+        }
+        private async Task SelectPrestamo(PrestamoClienteUI prestamo)
+        {
+          
+            if (MudDialog != null)
+            {
+                MudDialog.Close(DialogResult.Ok(prestamo));
+            }
+        }
+    }
+}
