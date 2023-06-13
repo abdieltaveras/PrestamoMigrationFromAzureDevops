28b7e7fe289ef935c1336c3dce090814ffef9c3f
Randy Castillo <randyjus25@hotmail.com>
2023-06-13 09:39:55 -0400

    edit createoreditcleinte

---
+++ PrestamoBlazorApp/Pages/Clientes/ImageDialog.razor.cs
@@ -0,0 +1,21 @@
+﻿using PrestamoEntidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PrestamoBlazorApp.Services;
+using Microsoft.AspNetCore.Components;
+using PrestamoBlazorApp.Shared;
+using MudBlazor;
+
+namespace PrestamoBlazorApp.Pages.Clientes
+{
+    public partial class ImageDialog : BaseForCreateOrEdit
+    {
+        [Inject]
+        IDialogService DialogService { get; set; }
+
+        [Inject]
+        ClientesService clientesService { get; set; }
+    }
+}
