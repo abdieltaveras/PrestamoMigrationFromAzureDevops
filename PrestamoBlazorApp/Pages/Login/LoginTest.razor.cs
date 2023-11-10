16518aa757f781485bcc560e85d50fb4a314ea22
Randy Castillo <randyjus25@hotmail.com>
2023-11-10 10:23:32 -0400

    [add]login sites test

---
+++ PrestamoBlazorApp/Pages/Login/LoginTest.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using PrestamoBlazorApp.Services;
+using PrestamoBlazorApp.Shared;
+using PrestamoEntidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+public class Users
+{
+    private string nombre { get; set; }
+    private string Contraseña { get; set; }
+}
+    public partial class LoginTest : BaseForList
+    {
+   
+
+
+    }
+
+
