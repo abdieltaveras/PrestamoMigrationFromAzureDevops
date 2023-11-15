2ecf01bee43b548338a333c6f58ee62a92f64bb2
abdieltaveras@hotmail.com <abdieltaveras@hotmail.com>
2023-11-14 21:35:51 -0400

    [add] onChange methods to update UI

--- PrestamoBlazorApp/Pages/Login/loginV2.razor.cs
+++ PrestamoBlazorApp/Pages/Login/loginV2.razor.cs
@@ -6,6 +6,7 @@ using PrestamoEntidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace PrestamoBlazorApp.Pages.Login
@@ -13,19 +14,41 @@ namespace PrestamoBlazorApp.Pages.Login
     public partial class LoginV2 : BaseForCreateOrEdit
     {
         private Compañias CompañiaSeleccionada { get; set; } = new Compañias();
-        private int LocalidadSeleccionada { get; set; }
-        public IEnumerable<Compañias> EmpresasList { get; set; } = new List<Compañias>();
 
+        private Localidad LocalidadSeleccionada { get; set; }
+        private int IdCompañiaSeleccionada { get; set; }
+        private int IdLocalidadSeleccionada { get; set; }
+        private IEnumerable<Compañias> EmpresasList { get; set; } = new List<Compañias>();
 
+        private IEnumerable<Localidad> LocalidadesDeLaEmpresa { get; set; } = new List<Localidad>();
         protected override Task OnInitializedAsync()
         {
             EmpresasList = new List<Compañias>
             {
-                new Compañias { IdEmpresa = 1, Empresa = "Intagsa", Localidades = new List <Localidad> { new Localidad {IdLocalidad=1, Nombre="Seibo" } } },
-                new Compañias { IdEmpresa = 2, Empresa = "Papito Prestamo", Localidades = new List<Localidad> { new Localidad { IdLocalidad = 2, Nombre = "Seibo" } } }
+                new Compañias { IdEmpresa = 1, Empresa = "Intagsa (varias localidades)", Localidades = new List <Localidad> {
+                    new Localidad {IdLocalidad=1, Nombre="La Romana" },
+                    new Localidad {IdLocalidad=2, Nombre="Rsj" },
+                    new Localidad {IdLocalidad=3, Nombre="Sosua" },
+                } },
+                new Compañias { IdEmpresa = 2, Empresa = "Motoprestamo Richiez (una sola localidad)", Localidades = new List<Localidad> { new Localidad { IdLocalidad = 4, Nombre = "Higuey" } } }
             };
             return base.OnInitializedAsync();
 
+        }
+
+        private async Task  CompaniaChanged(int idCompañia)
+        {
+            IdCompañiaSeleccionada = idCompañia;
+            var value = EmpresasList.FirstOrDefault(item => item.IdEmpresa == idCompañia);
+            
+            CompañiaSeleccionada = (value == null) ? null : value;
+            IdLocalidadSeleccionada = 0;
+            
+        }
+        private async Task LocalidadChanged(int idLocalidad)
+        {
+            IdLocalidadSeleccionada = idLocalidad;
+            LocalidadSeleccionada = CompañiaSeleccionada.Localidades.FirstOrDefault(item => item.IdLocalidad == idLocalidad); 
         }
     }
 }
@@ -37,8 +60,7 @@ public class Compañias
     public string Empresa { get; set; }
     public List<Localidad> Localidades { get; set; } = new List<Localidad>();
 
-
-    }
+}
 
 
 
