40041deac297c78a06229a1d8ee9fd8c2acfb235
CristalTaveras <cristaltaveras47@gmail.com>
2023-12-08 13:46:53 -0400

    [Add] spUpdateRfreshToken

---
+++ PrestamoDB/core/Stored Procedures/spUpdateRefreshToken.sql
@@ -0,0 +1,9 @@
+﻿CREATE procedure [core].[spUpdateRefreshToken]
+@UserName varchar(200),
+@RefreshToken varchar(max) 
+as
+begin
+	update [core].[tblUsers] 
+		set RefreshToken = @RefreshToken
+	WHERE UserName = @UserName
+end
