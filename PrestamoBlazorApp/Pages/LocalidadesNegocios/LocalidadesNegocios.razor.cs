15f30008fc96fd083b4f1a3abb591a559d1dcc0f
Randy Castillo <randyjus25@hotmail.com>
2024-01-26 16:18:37 -0400

    [add nombre de negocio en titulo de localidades ]

--- PrestamoBlazorApp/Pages/LocalidadesNegocios/LocalidadesNegocios.razor.cs
+++ PrestamoBlazorApp/Pages/LocalidadesNegocios/LocalidadesNegocios.razor.cs
@@ -14,15 +14,30 @@ namespace PrestamoBlazorApp.Pages.LocalidadesNegocios
     {
         [Inject]
         public LocalidadesNegociosService LocalidadesNegociosService { get; set; }
+        [Inject]
+        public NegociosService NegociosService { get; set; }
+        private IEnumerable<Negocio> negocios { get; set; } = new List<Negocio>();
 
         private LocalidadNegociosGetParams LocalidadNegociosGetParams { get; set; } = new LocalidadNegociosGetParams { Opcion = 1 };
         private IEnumerable<LocalidadNegocio> localidadesnegocios { get; set; } = new List<LocalidadNegocio>();
         private LocalidadNegocio LocalidadNegocio { get; set; } = new LocalidadNegocio();
         public int IdLocalidadNegocio { get; set; } = -1;
+        public string Nombrecomercial { get; set; }
         protected override async Task OnInitializedAsync()
         {
+            negocios = await NegociosService.Get(new NegociosGetParams());
             localidadesnegocios = await LocalidadesNegociosService.Get(new LocalidadNegociosGetParams { Opcion = 1 });
+
+            var value = negocios.FirstOrDefault(item => item.NombreComercial != null);
+            Nombrecomercial = value.NombreComercial;
+
         }
+        //protected async Task SelectedNombreNegocio()
+        //{
+            
+
+            
+        //}
 
         public virtual void OnEditClick(string url)
         {
