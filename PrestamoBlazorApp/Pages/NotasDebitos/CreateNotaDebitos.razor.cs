89030d41d96d4a21ff88c319b0c64eaaa16e4c69
Randy Castillo <randyjus25@hotmail.com>
2024-02-14 22:44:52 -0400

    add[add notas ]

---
+++ PrestamoBlazorApp/Pages/NotasDebitos/CreateNotaDebitos.razor.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Identity;
+using MudBlazor;
+using PrestamoBlazorApp.Models;
+using PrestamoBlazorApp.Services;
+using PrestamoBlazorApp.Shared;
+using PrestamoEntidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrestamoBlazorApp.Pages.NotasDebitos
+{
+    public partial class CreateNotaDebitos : BaseForCreateOrEdit
+    {
+        [Inject]
+        ClientesService ClientesService { get; set; }
+        [Inject]
+        PrestamosService PrestamosService { get; set; }
+        NotaDeDebito NotaDeDebito { get; set; } = new NotaDeDebito();
+        private PrestamoEntidades.Cliente ClienteSelected { get; set; } = new PrestamoEntidades.Cliente();
+        private PrestamoEntidades.Prestamo PrestamoSelected { get; set; } = new PrestamoEntidades.Prestamo();
+        public int IdPrestamo { get; set; } = -1;
+        private async Task GetDataPrestamo()
+        {
+            PrestamoSelected = new PrestamoEntidades.Prestamo();
+           var prestamos = await PrestamosService.GetAsync(new PrestamoEntidades.PrestamosGetParams { idPrestamo = IdPrestamo });
+            PrestamoSelected = prestamos.FirstOrDefault();
+            IdPrestamo = PrestamoSelected.IdPrestamo;
+            
+            StateHasChanged();
+        }
+
+
+
+
+
+
+
+    }
+}
+ 
+
+
+
+
+
+
+
