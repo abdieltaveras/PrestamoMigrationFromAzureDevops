460b9077f2c9f3d0784a91db1426f945c81ad77a
Randy Castillo <randyjus25@hotmail.com>
2024-04-03 09:09:29 -0400

    fix[modificando cargos ]

--- PrestamoBlazorApp/Pages/NotasDebitos/DetallesCargos.razor.cs
+++ PrestamoBlazorApp/Pages/NotasDebitos/DetallesCargos.razor.cs
@@ -14,23 +14,19 @@ using static MudBlazor.CategoryTypes;
 
 namespace PrestamoBlazorApp.Pages.NotasDebitos
 {
-    public partial class CreateNotaDebitos : BaseForCreateOrEdit
+    public partial class DetallesCargos : BaseForList
     {
         [Inject]
         ClientesService ClientesService { get; set; }
         [Inject]
         PrestamosService PrestamosService { get; set; }
         NotaDeDebito NotaDe { get; set; } = new NotaDeDebito();
-         private PrestamoEntidades.Cliente ClienteSelected { get; set; } = new PrestamoEntidades.Cliente();
-        private PrestamoEntidades.Prestamo PrestamoSelected { get; set; } = new PrestamoEntidades.Prestamo();
         public int IdPrestamo { get; set; } = -1;
         private string SelectedCodigo { get; set; }
         private IEnumerable<string> CargosSelected { get; set; } = new List<string>();
         private IEnumerable<CodigoCargos> CodigosCargos { get; set; } = new List<CodigoCargos>();
         private List<DetalleCargo> DataSelect { get; set; } = new List<DetalleCargo>();
-        
-        public bool estaEditando=true;
-        public decimal MontoCargado { get; set; }
+        public decimal MontoCargado { get; set; } = 0;
         public decimal MontoRestante { get; set; } = 0;
         protected override Task OnInitializedAsync()
         {
@@ -38,18 +34,7 @@ namespace PrestamoBlazorApp.Pages.NotasDebitos
             return base.OnInitializedAsync();
             
         }
-        private async Task GetDataPrestamo()
-        {
-
-
-            PrestamoSelected = new PrestamoEntidades.Prestamo();
-            var prestamos = await PrestamosService.GetAsync(new PrestamoEntidades.PrestamosGetParams { idPrestamo = IdPrestamo });
-            PrestamoSelected = prestamos.FirstOrDefault();
-            IdPrestamo = PrestamoSelected.IdPrestamo;
-
-            StateHasChanged();
-        }
-        
+       
         private async Task AgregarDetalle(MouseEventArgs arg)
         {
              AsignarCargosDebito();
@@ -63,20 +48,21 @@ namespace PrestamoBlazorApp.Pages.NotasDebitos
             {
                 CodigoCargo = SelectedCodigo,
                 Monto = MontoCargado,
-                //NombreCargo = cargos.Nombre
+                // NombreCargo = cargos.Nombre
                 NombreCargo = cargos != null ? cargos.Nombre : null
             };
+
             if (MontoCargado + detalleCargo.Monto <= NotaDe.Monto)
             {
                 DataSelect.Add(detalleCargo);
-                MontoCargado += detalleCargo.Monto;
+                MontoCargado = detalleCargo.Monto;
                 MontoRestante = NotaDe.Monto - detalleCargo.Monto;
-
             }
+
             else
             {
                 _ = NotifyMessageBySnackBar("Monto Excedido, Verificar Monto Asignado", MudBlazor.Severity.Error);
-                MontoCargado = 0;
+                //MontoCargado = 0;
             }
         }
 
