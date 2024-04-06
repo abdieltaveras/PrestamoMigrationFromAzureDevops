e6f0c30d999e4632035f0ad1ad48709a4d923ae9
Luis Heskey <luisbillr@hotmail.com>
2024-04-06 18:52:30 -0400

    [added]added new codigos

--- PrestamoBLL/CodigosCargosDebitosReservadosBLL.cs
+++ PrestamoBLL/CodigosCargosDebitosReservadosBLL.cs
@@ -14,10 +14,58 @@ namespace PrestamoBLL
         {
             
         }
+        public class CodigosCargosDebitosReservadosClass
+        {
 
+            public const string Capital = "CA";
+            public const string Interes = "INT";
+            public const string InteresDespuesDeVencido = "INTDV";
+            public const string Moras = "MOR";
+            public const string InteresDelGastoDeCierre = "INTGC";
+            public const string GastoDeCierre = "GC";
+            public const string OtrosCargos = "OC";
+            public const string InteresOtrosCargos = "INTOC";
+            public static string GetNombre(string codigoCargo)
+            {
+
+                string nombre = string.Empty;
+                switch (codigoCargo)
+                {
+                    case Capital: nombre = "Capital"; break;
+                    case Interes: nombre = "Interes"; break;
+                    case InteresDespuesDeVencido: nombre = "Interes despues de vencido"; break;
+                    case Moras: nombre = "Moras (Cargos por atraso)"; break;
+                    case GastoDeCierre: nombre = "Gasto de cierre"; break;
+                    case InteresDelGastoDeCierre: nombre = "Interes del gasto de cierre"; break;
+                    case OtrosCargos: nombre = "Otros  Cargos"; break;
+                    case InteresOtrosCargos: nombre = "Interes Otros cargos"; break;
+                    default: nombre = "el codigo indicado no esta registrado"; break;
+                }
+                return nombre;
+            }
+            public override string ToString() => "Codigos reservados para utilizar en los cargos";
+            public static IEnumerable<string> GetCodigos()
+            {
+                // no se incluye otros cargos porque ese reservados para agrupar los cargos
+                // creados por el usuario
+                var codigosCargos = new List<string>()
+            {
+                Capital, Interes, InteresDespuesDeVencido, Moras, GastoDeCierre, InteresDelGastoDeCierre, InteresOtrosCargos
+            };
+                return codigosCargos;
+            }
+        }
         public IEnumerable<CodigosCargosDebitosReservados> Get(CodigosCargosGetParams searchParam)
         {
-            return this.Get<CodigosCargosDebitosReservados>("spGetCodigosCargos", searchParam);
+            List<CodigosCargosDebitosReservados> ls = new List<CodigosCargosDebitosReservados>();
+            ls =  this.Get<CodigosCargosDebitosReservados>("spGetCodigosCargos", searchParam).ToList();
+            var codes = CodigosCargosDebitosReservadosClass.GetCodigos();
+            foreach (var code in codes)
+            {
+                ls.Add(new CodigosCargosDebitosReservados { Codigo = code, 
+                    Nombre = CodigosCargosDebitosReservadosClass.GetNombre(code), Usuario="SISTEMA" });
+            }
+            return ls;
         }
         public int InsUpd(CodigosCargosDebitosReservados insUpdParam)
         {
